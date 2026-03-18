6768f55afd73d050d92da6d0429058c3ad8647f6
Jemarco Briz <jemarcobriz123@gmail.com>
2026-03-18 16:08:54 +0800

    Import UI components into index file

---
+++ src/pages/Index.tsx
@@ -0,0 +1,23 @@
+import Navigation from "@/components/Navigation";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import SkillsSection from "@/components/SkillsSection";
+import FooterSection from "@/components/FooterSection";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navigation />
+      <HeroSection />
+      <AboutSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <SkillsSection />
+      <FooterSection />
+    </div>
+  );
+};
+
+export default Index;
